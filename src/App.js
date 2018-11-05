79e8110578dbd7b31a05a095d185975baee12104
Bruno Correia <brunocs@msn.com>
2018-11-04 23:57:36 -0200

    Apply context to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import * as themes from "./themes";
+import ThemeContext from "./themes/context";
 import Counter from "./components/Counter";
 
 class App extends Component {
+  state = {
+    theme: themes.corporate
+  };
+
   render() {
     return (
       <div className="App">
-        <Counter />
+        <ThemeContext.Provider value={this.state}>
+          <ThemeContext.Consumer>{theme => <Counter />}</ThemeContext.Consumer>
+        </ThemeContext.Provider>
       </div>
     );
   }
